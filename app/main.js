e3560dc714d88886c2e68dd081e4f7baf0329ef0
sanemat <o.gata.ken@gmail.com>
2014-02-21 16:42:46 +0900

    Add logic

--- app/main.js
+++ app/main.js
@@ -1,8 +1,4 @@
 /*jshint strict:false, browser:true */
 (function bookmarklet() {
-  var styleNode = document.createElement('style'),
-    content = document.createTextNode('body { background: cornflowerblue; }');
-
-  styleNode.appendChild(content);
-  document.head.appendChild(styleNode);
+  $('html').css('-webkit-filter', 'grayscale(1)');
 }());
